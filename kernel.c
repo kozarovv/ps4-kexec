fd707617e990a263d2aca7001bd951afd39c8c79
kozarovv <15552250+kozarovv@users.noreply.github.com>
2019-02-10 15:59:39 +0100

    Fixed pmap_protect check, thanks to zecoxao

--- kernel.c
+++ kernel.c
@@ -254,7 +254,7 @@ static int patch_pmap_check(void)
 
     for (p = (u8*)kern.pmap_protect;
          p < ((u8*)kern.pmap_protect + 0x500); p++) {
-        #ifdef PS4_5_05
+        #ifdef PS4_4_74
         if (!memcmp(p, "\xB8\x06\x00\x00\x00\xC4", 6)) {
             p[1] = 0;
             kern.printf("pmap_protect patch successful (found at %p)\n", p);
